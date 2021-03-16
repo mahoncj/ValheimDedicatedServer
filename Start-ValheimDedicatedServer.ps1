51c713c107ef2ea0eb447d710ffc5c3291124355
Mahon, Christopher <Christopher.Mahon@emc.com>
2021-03-16 01:00:01 -0400

    Update Start-ValheimDedicatedServer.ps1

--- Start-ValheimDedicatedServer.ps1
+++ Start-ValheimDedicatedServer.ps1
@@ -1,368 +1,170 @@
 <#
-
 .SYNOPSIS
-    
-    Starts the Valheim Dedicated Server process
+    Valheim Server Management Script.
+    This script will help you easily install your valheim server on a windows machine utilizing SteamCMD.
+    Prerequisites:
+        1) Install SteamCMD from their website (https://developer.valvesoftware.com/wiki/SteamCMD#Downloading_SteamCMD)
+        2) Update the Script with the correct locations of the directories you'll use
+
+    Execution:
+        This script has been cut up into functions so you can execute only sections of it.
+        Without any parameters, the script will execute any bacukps, update the game, validate the game files, start the server.
+        You can call it with parametrs to only do the update + validate, backup, or start the server. Call the script with the -help parameter to see available usages.
 
 .EXAMPLE
-
-    .\Start-ValheimDedicatedServer.ps1 -PathToSteamCMD C:\steamcmd -PathToValheimBackup C:\valheimbackup -PathToValheimInstall C:\valheim
-
+    .\Start-ValheimServer
+    .\Start-ValheimServer -Help
 .LINK
-
     Author:    Chris Mahon
     Email:     chrismahon@blueflashylights.com
     Release:   20210218
-
 #>
+param(
+[Alias('b')]
+[switch]$backup,
+[Alias('d')]
+[switch]$debug,
+[Alias('h')]
+[switch]$help,
+[Alias('i')]
+[switch]$install,
+[Alias('s')]
+[switch]$start,
+[Alias('u')]
+[switch]$update
+)
 
-[CmdletBinding()]
-Param (
 
-    [parameter(
-        mandatory=$true,
-        position=0)
-    ]
-    [alias('steamCmd')]
-    [string] $PathToSteamCMD=$null,
+####################################################
+#Server Setup - Update these with the correct values for your server
+####################################################
 
-    [parameter(
-        mandatory=$true,
-        position=1)
-    ]
-    [alias('valheimBackup')]
-    [string] $PathToValheimBackup=$null,
+$steamCmd = "C:\SteamCmd\steamcmd.exe";
+$valheimDir = "C:\SteamApps\Valheim\"
+$valheimBkup = "C:\SteamApps\ValheimBackup\"
+$valheimSaves = "C:\Users\{ServerLogin}\AppData\LocalLow\IronGate\Valheim\"
 
-    [parameter(
-        mandatory=$true,
-        position=2)
-    ]
-    [alias('valheimInstall')]
-    [string] $PathToValheimInstall=$null
+$gameName = "Whatever You'd Like";
+$worldName = "Name of your World";
+$gamePass = "MUST BE 6+ Characters Long";
 
-);
 
-$ErrorActionPreference = 'Stop';
+####################################################
+#Functions used in the script
+#Don't update these unless you know what you're doing
+####################################################
 
-function ui_start_status ($message) {
 
-    write-host;
-    write-host ("$($message) ... ".PadRight(114)) -NoNewline;
-
+function Start-Valheim(){
+    Set-Item ENV:SteamAppId 892970
+    Write-Update "Starting Valheim Server, press Ctrl+c to close server";
+    $cmd = "$($valheimDir)valheim_server.exe"
+    $params = "-nographics -batchmode -name `"$gameName`" -port 2456 -world `"$worldName`" -password `"$gamePass`""
+    Write-Debug "Executing '$cmd $params'"
+    Invoke-Expression "$cmd $params"
 }
 
-function ui_complete_status () {
-
-    write-host '[OK]' -foregroundcolor green;
-
+function Update-Valheim(){
+    $params = "+login anonymous +force_install_dir $($valheimDir) +app_update 896660 validate +exit";
+    Write-Debug "Executing '$steamCmd $params'"
+    Invoke-Expression "$steamCmd $params"
 }
 
-function ui_error_status ($message = $null) {
+function Write-Update($msg){
+    Write-Host -ForegroundColor Yellow $msg;
+}
+function Write-Done(){
+    $width = (Get-Host).UI.RawUI.MaxWindowSize.Width;
+    Write-Host -ForegroundColor Green "[Doneski]";
+    Write-Host "$("-" * $width)`n`n";
+}
 
-    write-host "[ERR]" -foregroundcolor red;
-    write-host;
+function Write-Short-Error ($message = $null) {
+    Write-Host -ForegroundColor Red "[ERR]`n";
 
     if ($null -ne $message) {
-
-        write-host $message -foregroundcolor red;
-        write-host;
-
-    }
-
-}
-
-function ui_write_header ($title) {
-
-    clear-host;
-
-    $lines = @();
-    $lines += "START VALHEIM DEDICATED SERVER V2.0";
-    $lines += "";
-    $lines += "THIS SCRIPT IS STILL IN DEVELOPMENT";
-    
-    write-host;
-    write-host;
-
-    $color = "yellow";
-
-    foreach ($line in $lines) {
-
-        write-host "$($line)".padleft(($host.ui.rawui.windowsize.width / 2) + $line.length - ($line.length / 2)) -foregroundcolor $color;
-
-        $color = "gray";
-
-    }
-    
-    write-host;
-    write-host;
-    write-host;
-
-    write-host $title -foregroundcolor magenta;
-
-}
-
-function backup_valheim_files () {
-
-    if ($PathToValheimBackup) {
-
-        $date = get-date -format 'yyyyMMddHHmm';
-
-        $irongate = $home + '\AppData\LocalLow\IronGate';
-    
-        $TestIrongate = test-path -path $Irongate;
-    
-        if ($TestIrongate -eq $true) {
-    
-            copy-item -path $Irongate -recurse -destination "$PathToValheimBackup\Backups\$Date\IronGate";
-        }
-
-        else {
-    
-            write-host;
-            write-host "Could not find the IronGate folder containing your worlds. If this is not a fresh install, please check that the IronGate folder exists within $home\AppData\LocalLow." -foregroundcolor yellow;
-            write-host;
-
-        }
-    
-    }
-
-}
-
-function check_steamcmd () {
-
-    $TestSteamCMD = test-path -path "$PathToSteamCMD\steamcmd.exe";
-
-    if ($TestSteamCMD -ne $true) {
-
-        write-host;
-        write-host "SteamCMD was not found within ""$PathToSteamCMD"". Would you like to download it?" -foregroundcolor yellow;
-        write-host;
-    
-        $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "This will download SteamCMD to ""$PathToSteamCMD"".";
-    
-        $quit = New-Object System.Management.Automation.Host.ChoiceDescription "&Quit", "This will terminate the script.";
-    
-        $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $quit);
-    
-        $result = $Host.ui.PromptForChoice($null, $null, $options, 0);
-    
-            if ($result -eq 1) {
-    
-                exit;
-    
-            }
-
-        $null = new-item -itemtype directory -Path $PathToSteamCMD;
-
-        $null = invoke-webrequest -uri "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip" -method "GET"  -outfile "$PathToSteamCMD\steamcmd.zip";
-
-        $null = expand-archive -path "$PathToSteamCMD\steamcmd.zip" -DestinationPath $PathToSteamCMD;
-        
-    }
-
-}
-
-function update_valheim () {
-
-    $TestValheimInstall = test-path -path "$PathToValheimInstall";
-
-    if ($TestValheimInstall -eq $true) {
-
-        $currentdirectory = get-location;
-
-        $null = set-location -path $PathToSteamCMD;
-
-        $null = .\steamcmd.exe +login anonymous +force_install_dir $PathToValheimInstall +app_update 896660 validate +exit;
-    
-        $null = set-location -path $currentdirectory;
-
-    }
-
-    else {
-
-        write-host;
-        write-host "Valheim Dedicated Server was not found within ""$PathToValheimInstall"". Would you like to download it?" -foregroundcolor yellow;
-        write-host;
-    
-        $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "This will download SteamCMD to ""$PathToSteamCMD"".";
-    
-        $quit = New-Object System.Management.Automation.Host.ChoiceDescription "&Quit", "This will terminate the script.";
-    
-        $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $quit);
-    
-        $result = $Host.ui.PromptForChoice($null, $null, $options, 0);
-    
-            if ($result -eq 1) {
-    
-                exit;
-    
-            }
-
-        $currentdirectory = get-location;
-        
-        $null = set-location -path $PathToSteamCMD;
-
-        $null = .\steamcmd.exe +login anonymous +force_install_dir $PathToValheimInstall +app_update 896660 validate +exit;
-
-        $null = set-location -path $currentdirectory;
-        
-        $script:NewValheimInstall = $true;
-    }
-
-}
-
-function backup_headless_server() {
-
-    $TestHeadlessServer = test-path -path "$PathToValheimInstall\start_headless_server.bat";
-
-    if ($TestHeadlessServer -eq $true) {
-
-        $null = copy-item -path "$PathToValheimInstall\start_headless_server.bat" -destination $PathToValheimBackup;
-    }
-
-    else {
-
-        write-host;
-        write-host "The start_headless_server.bat file was not located within ""$PathToValheimInstall"" directory." -foregroundcolor yellow;
-        write-host;
-
-    }
-
-}
-
-function restore_headless_server () {
-
-    $TestHeadlessServerBackup = test-path -path "$PathToValheimBackup\start_headless_server.bat";
-
-    if ($TestHeadlessServerBackup -eq $true) {
-
-        $null = copy-item -path "$PathToValheimBackup\start_headless_server.bat" -destination "$PathToValheimInstall\start_headless_server.bat";
-    }
-
-    else {
-
-        write-host;
-        write-host "No previous start_headless_server.bat file detected within the ""$PathToValheimBackup"" directory. This is a fresh install." -foregroundcolor yellow;
-        write-host;
+        Write-Host -ForegroundColor DarkRed "$($message)`n";
     }
 }
 
-function check_valheim_firewall_rules () {
+function Write-Header ($msg) {
+    $width = (Get-Host).UI.RawUI.MaxWindowSize.Width;
+    Write-Debug "Got width of $width"
 
-    $ValheimTCPInbound = Get-NetFirewallRule -Name "VALHEIM_TCP_IN" -ErrorAction SilentlyContinue;
-
-    if ($ValheimTCPInbound.Enabled -ne "True") {
-
-        write-host;
-        write-host "Adding the following Firewall rule: ""VALHEIM_TCP_IN"" - Inbound - TCP - 2456-2458" -foregroundcolor green;
-        write-host;
-        $null = New-NetFirewallRule -Name "VALHEIM_TCP_IN" -DisplayName "VALHEIM_TCP_IN" -Direction Inbound -LocalPort 2456-2458 -Protocol TCP -Action Allow -ErrorAction SilentlyContinue;
-
-    }
-    
-    $ValheimUDPInbound = Get-NetFirewallRule -Name "VALHEIM_UDP_IN" -ErrorAction SilentlyContinue;
-
-    if ($ValheimUDPInbound.Enabled -ne "True") {
-
-        write-host;
-        write-host "Adding the following Firewall rule: ""VALHEIM_UDP_IN"" - Inbound - UDP - 2456-2458" -foregroundcolor green;
-        write-host;
-        $null = New-NetFirewallRule -Name "VALHEIM_UDP_IN" -DisplayName "VALHEIM_UDP_IN" -Direction Inbound -LocalPort 2456-2458 -Protocol UDP -Action Allow -ErrorAction SilentlyContinue;
-    
-    }
-
-    $ValheimTCPOutbound = Get-NetFirewallRule -Name "VALHEIM_TCP_OUT" -ErrorAction SilentlyContinue;
-
-    if ($ValheimTCPOutbound.Enabled -ne "True") {
-
-        write-host;
-        write-host "Adding the following Firewall rule: ""VALHEIM_TCP_OUT"" - Outbound - TCP - 2456-2458" -foregroundcolor green;
-        write-host;
-        $null = New-NetFirewallRule -Name "VALHEIM_TCP_OUT" -DisplayName "VALHEIM_TCP_OUT" -Direction Outbound -LocalPort 2456-2458 -Protocol TCP -Action Allow -ErrorAction SilentlyContinue;
-    
-    }
-    
-    $ValheimUDPOutbound = Get-NetFirewallRule -Name "VALHEIM_UDP_OUT" -ErrorAction SilentlyContinue;
-
-    if ($ValheimUDPOutbound.Enabled -ne "True") {
-
-        write-host;
-        write-host "Adding the following Firewall rule: ""VALHEIM_UDP_OUT"" - Outbound - UDP - 2456-2458" -foregroundcolor green;
-        write-host;
-        $null = New-NetFirewallRule -Name "VALHEIM_UDP_OUT" -DisplayName "VALHEIM_UDP_OUT" -Direction Outbound -LocalPort 2456-2458 -Protocol UDP -Action Allow -ErrorAction SilentlyContinue;
-
-    }
-
+   
+    Write-Host "`n$("=" * $width)";
+    Write-Host -ForegroundColor Magenta $msg
+    Write-Host "$("-" * $width)`n`n";
 }
+
+function Backup-Valheim-Files ($src, $dst) {
+    $date = get-date -format 'yyyyMMddHHmm'
+    $bkupDir = $dst + "\" + $date;
+
+    if ((Test-Path $src) -and (Test-Path $dst) -and (-not (Test-Path $bkupDir))){
+        New-Item -ItemType Directory -Path $dst -Name $date;
+        Copy-Item -path $src -recurse -destination $bkupDir;
+    } else {
+        $errMsg = "";
+        if (-not (Test-Path $src)) { $errMsg += "Could not find $src `r`n"; }
+        if (-not (Test-Path $dst)) { $errMsg += "Could not find $dst `r`n"; }
+        if ((Test-Path $bkupDir)) { $errMsg += "$bkupDir exists. Did you create a backup recently? `r`n"; }
+        Write-Short-Error "$errMsg `r`nPlease fix the issues and/or update the script variables";
+    }
+}
+
+
+####################################################
+#Start Script
+####################################################
+
+Clear-Host
+
+
+Write-Header "Valheim Updater";
+if ($help){
+    Write-Host "This is a quick script to manage your Valheim server via powershell.`r`n";
+
+    Write-Host "`t-B -Backup`tCreates a backup of the game files (worlds)";
+    Write-Host "`t-D -Debug`tPrints out helpful statements";
+    Write-Host "`t-H -Help`tPrints out this menu";
+    Write-Host "`t-I -Install`tInstalls and verifies the game using SteamCMD";
+    Write-Host "`t-S -Start`tStarts the server";
+    Write-Host "`t-U -Update`tUpdates and verifies the game using SteamCMD";
+    Write-Host "`t`t`t(No Flags) Runs a backup, then attempts to update, finally starts the server";
+    return;
+}
+if ($debug){ $DebugPreference = 'Continue'; Write-Debug "Switches: Install: $install Update: $update Start: $start Debug: $debug"; }
+
+if ($backup){
+    Backup-Valheim-Files $valheimSaves $valheimBkup;
+    Write-Done;
+    return;
+}
+if ($install -or $update){
+    Update-Valheim;
+    Write-Done;
+    return;
+}
+if ($start){
+    Start-Valheim;
+    Write-Done;
+    return;
+}
+
 
 try {
-
-    ### Perform Start Valheim Dedicated Server
-
-    ui_write_header "Valheim Dedicated Server - Start Server";
-
-    ui_start_status "Checking for SteamCMD";
-
-    check_steamcmd;
-
-    ui_complete_status;
-
-    ui_start_status "Backing up IronGate directory to ""$PathToValheimBackup""";
-
-    backup_valheim_files;
-
-    ui_complete_status;
-
-    ui_start_status "Backing up start_headless_server.bat file to ""$PathToValheimBackup""";
-
-    backup_headless_server;
-
-    ui_complete_status;
-    
-    ui_start_status "Updating Valheim Dedicated Server";
-
-    update_valheim;
-
-    ui_complete_status;
-
-    ui_start_status "Restoring existing Valheim start_headless_server.bat file to ""$PathToValheimInstall""";
-
-    restore_headless_server;
-
-    ui_complete_status;
-
-    ui_start_status "Checking Valheim Dedicated Server firewall rules";
-
-    check_valheim_firewall_rules;
-
-    ui_complete_status;
-
-    ui_start_status "Starting Valheim Dedicated Server";
-
-    if ($script:NewValheimInstall -eq $true) {
-
-        write-host;
-        write-host "New Valheim Dedicated Install located within ""$PathToValheimInstall"". Please edit the start_headless_server.bat file within ""$PathToValheimInstall"" to provide your world details and password and run this script again!" -ForegroundColor yellow;
-        write-host;
-        exit;
-    }
-
-    $currentdirectory = get-location;
-
-    $null = set-location -path $PathToValheimInstall;
-
-    $null = start-process .\start_headless_server.bat;
-
-    $null = set-location -path $currentdirectory;
-
-    ui_complete_status;
-
+    Write-Update "Backing up data";
+    Backup-Valheim-Files $valheimSaves $valheimBkup;
+    Write-Done;
+    Write-Update "Updating Valheim";
+    Update-Valheim;
+    Write-Done;
+    Write-Update "Starting Valheim";
+    Start-Valheim;
+    Write-Done;
+    Write-Done;
 } catch {
-
-    ui_error_status $_.exception.message;
-
-    read-host "Press any key to exit...";
-
-    write-host;
-
+    Write-Short-Error $_.Exception.Message;
+    Read-Host "Press the any key to exit";
 }
